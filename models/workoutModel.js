412cc2c53e835a456eab6f15fb655e81b4aba95f
jeffreyadamo <jeffreyadamo@gmail.com>
2020-06-30 18:32:42 -0700

    cleaned up some extraneous code

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -10,52 +10,39 @@ const workoutsSchema = new Schema({
   exercises: 
   [
       {
-    //      type: Schema.Types.Array,
-        //  ref: "exercises"
         type : {
             type: String,
-            // trim: true,
             required: "Please select an exercise type"
         },
     
         name: {
             type: String,
-            // trim: true,
             required: "Name is Required"
           },
-          
         duration: {
             type: Number,
-            // trim: true,
             required: "Duration is Required"
         },
         distance: {
             type: Number,
-            // trim: true,
-
         },
         weight: {
             type: Number,
-            // trim: true,
-
         },
-    
         reps: {
             type: Number,
-            // trim: true,
-
         },
         sets: {
             type: Number,
-            // trim: true,
-
         }
       }
    ]
 },
-{toJSON: {
+{
+    toJSON: {
     virtuals: true
-}});
+    }
+});
 
 workoutsSchema.virtual("totalDuration").get(function(){
     return this.exercises.reduce(function(total,exercise){
--- server.js
+++ server.js
@@ -25,19 +25,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 app.use(require("./routes/api-routes.js"));
 require("./routes/html-routes.js")(app);
 
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/public/index.html"));
-// })
-
-// app.get("/stats", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/public/stats.html"));
-// })
-
-// app.get("/exercise", (req, res) => {
-//     res.sendFile(path.join(__dirname, "/public/exercise.html"));
-// })
-
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
